a3addaf90f2424f71c55bd96d69e91e6913b9cc4
Yunus Söyleyici <yunus.soeyleyici107@gmail.com>
2026-06-20 22:09:38 +0200

    build(cmake) configure basic arm-none-eabi options for Cortext-M4

---
+++ arm-none-eabi.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+
+set(CPU_FLAGS "-mcpu=cortex-m4 -mthumb")
+set(CMAKE_C_FLAGS_INIT "${CPU_FLAGS}")
